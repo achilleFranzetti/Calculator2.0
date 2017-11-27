a3c60e5e7f201899e531433756d24d0bb14747f9
Achille Franzetti <achille.franzetti@econocom.com>
2017-11-27 20:41:21 +0100

    Initial Commit

---
+++ Calculator2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator2.0
+//
+//  Created by FRANZETTI-LAPTOP on 27/11/17.
+//  Copyright © 2017 FRANZETTI-LAPTOP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
